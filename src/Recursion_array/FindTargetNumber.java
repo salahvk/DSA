0b985a5b9aaec4fb403b8d21741804cc2513b140
Salah <salah@viewydigital.com>
2024-02-12 23:13:51 +0530

    findAllTargetIndexes function added

--- src/Recursion_array/FindTargetNumber.java
+++ src/Recursion_array/FindTargetNumber.java
@@ -1,14 +1,30 @@
 package Recursion_array;
+
+import java.util.ArrayList;
+
 public class FindTargetNumber {
     public static void main(String[] args) {
-        int[] arr = {1,4,6,7,8};
-        System.out.println(isTargetfounded(arr,6, 0));
+        int[] arr = { 1, 4, 6, 7, 8 };
+        System.out.println(isTargetfounded(arr, 6, 0));
+
+        int[] arr1 = { 1, 4, 6, 7, 8, 7, 4, 4 };
+        System.out.println(findAllTargetIndexes(arr1, 4, 0,new ArrayList<>()));
     }
 
- static   boolean isTargetfounded(int[] arr, int target, int index) {
+    static boolean isTargetfounded(int[] arr, int target, int index) {
         if (arr.length - 1 == index) {
             return false;
         }
         return (arr[index] == target) || isTargetfounded(arr, target, ++index);
+    }
+
+    static ArrayList<Integer> findAllTargetIndexes(int[] arr, int target, int index, ArrayList<Integer> list) {
+        if (arr.length == index) {
+            return list;
+        }
+        if(arr[index] == target){
+            list.add(index);
+        }
+        return findAllTargetIndexes(arr, target, ++index, list);
     }
 }

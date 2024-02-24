e8423aa088e4a1864f4cb36d76d34fe758504cf4
Salah <salah@viewydigital.com>
2024-02-24 17:19:35 +0530

    deleting a node from linked list

--- src/brototype/SLinkedList.java
+++ src/brototype/SLinkedList.java
@@ -30,19 +30,46 @@ public class SLinkedList {
             return;
         }
         Node temp = head;
-        while (temp != null){
+        while (temp != null) {
             System.out.println(temp.data);
             temp = temp.next;
         }
     }
+
+    public void delete(int data) {
+        Node temp = head, prev = null;
+        if (temp != null && temp.data == data) {
+            head = temp.next;
+            return;
+        }
+        while (temp != null && temp.data != data) {
+            prev = temp;
+            temp = temp.next;
+        }
+        if (temp == null) {
+            System.out.println("Nothing to delete");
+            return;
+        }
+
+        if (temp == tail) {
+            tail = prev;
+            tail.next = null;
+            return;
+        }
+
+        prev.next = temp.next;
+
+    }
+
     public static void main(String[] args) {
         SLinkedList list = new SLinkedList();
         list.displayNode();
 
         list.addNode(0);
-        list.addNode(1);
+        list.addNode(8);
         list.addNode(2);
         list.addNode(100);
+        list.delete(100);
 
         list.displayNode();
     }

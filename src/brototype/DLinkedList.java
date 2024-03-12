a850486acff8d64c52fbbaeb81a72029d4288069
salahvk <salahkoyilandy@gmail.com>
2024-03-12 23:41:07 +0530

    remove duplicate item of doubly linked list added

--- src/brototype/DLinkedList.java
+++ src/brototype/DLinkedList.java
@@ -85,17 +85,38 @@ public class DLinkedList {
 
     }
 
+    void removeDuplicate() {
+        // loop all over the list to find a value that equals the next values to it
+        // if we got a value cut the links before and after
+        System.out.println("Removing duplicate function executed");
+        Node temp = headNode;
+
+        while (temp != null) {
+            temp = temp.next;
+            // if (temp == null) {
+            //     return;
+            // }
+            // Node previous = temp.previous;
+            // while (temp.data == previous.data) {
+            //     previous.next = temp.next;
+            // }
+        }
+        // System.out.println(temp.data);
+    }
+
     public static void main(String[] args) {
         DLinkedList list = new DLinkedList();
         list.addNode(1);
         list.addNode(5);
+        list.addNode(5);
+        list.addNode(5);
+        list.addNode(8);
         list.addNode(8);
         list.addNode(12);
         list.addNode(15);
         // list.delete(8);
-        // list.insertAfter(15, 100);
-        // list.insertAfter(100, 100);
-        list.insertAfter(100, 5);
+        list.insertAfter(12, 9);
+        list.removeDuplicate();
         list.displayNode();
         // list.displayNodeInReverse();
     }

82c4db32da8467ad946cdd6d50571feadb591156
salahvk <salahkoyilandy@gmail.com>
2024-06-03 21:58:09 +0530

    ceiling of char problem solved

---
+++ src/leetcode/_744/CeilingOfChar.java
@@ -0,0 +1,23 @@
+package leetcode._744;
+
+public class CeilingOfChar {
+    public static void main(String[] args) {
+        char[] letters = { 'c', 'f', 'j' };
+        char target = 'a';
+        System.out.println(nextGreatestLetter(letters, target));
+    }
+
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+}

094f9345f7be6e58110e8085533175bdc89ced88
salahvk <salahkoyilandy@gmail.com>
2024-06-25 08:27:23 +0530

    right most set bit solution

--- src/kunal_kushwaha/bitwise_operators/RightmostSetBit.java
+++ src/kunal_kushwaha/bitwise_operators/RightmostSetBit.java
@@ -2,9 +2,14 @@ package kunal_kushwaha.bitwise_operators;
 
 public class RightmostSetBit {
     public static void main(String[] args) {
-        int n = 19;
-        int m = -19;
+        int n = 2; // The binary is 10011 and right most digits position is 1st
         System.out.println(Integer.toBinaryString(n));
-        System.out.println(Integer.toBinaryString(m));
+        System.out.println(findsetBitPosition(n));
+
+    }
+
+    public static int findsetBitPosition(int num) {
+        int isolatedBit = num & -num;
+        return (int)(Math.log(isolatedBit)/Math.log(2) + 1);
     }
 }

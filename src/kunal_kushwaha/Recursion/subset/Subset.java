a7692b13460cf32f80575dc996d0ed2725952f96
salahvk <salahkoyilandy@gmail.com>
2024-07-14 05:29:57 +0530

    subset problem solved

---
+++ src/kunal_kushwaha/Recursion/subset/Subset.java
@@ -0,0 +1,17 @@
+package kunal_kushwaha.recursion.subset;
+
+public class Subset {
+    public static void main(String[] args) {
+        subset("", "abc");
+    }
+
+    public static void subset(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+        char firstChar = up.charAt(0);
+        subset(p, up.substring(1));
+        subset(p + firstChar , up.substring(1));
+    }
+}

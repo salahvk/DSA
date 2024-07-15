0fbd83f0b22308730d00234d6f773b6506e749de
salahvk <salahkoyilandy@gmail.com>
2024-07-15 16:29:35 +0530

    leetcode 217 th problem solved

---
+++ src/leetcode/_217/ContainsDuplicate.java
@@ -0,0 +1,31 @@
+package leetcode._217;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] arr = { 3, 5, 6, 7, 7 };
+        System.out.println(containsDuplicate(arr));
+
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> uniques = new HashSet<>();
+        // hashset is a collection where it doesnt contain any duplicate numbers
+        // HashSet: [1, 2, 3] hashset prints like this
+
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // map.put(1, 100); we add maps like this
+
+        for (int i = 0; i < nums.length; i++) {
+            if (uniques.contains(nums[i])) {
+                return true;
+            } else {
+                uniques.add(nums[i]);
+            }
+        }
+        return false;
+    }
+}

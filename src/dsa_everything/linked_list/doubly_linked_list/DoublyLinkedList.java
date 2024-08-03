d00dab6d628f8c119a36edfc0bd0c2e2696d5e38
salahvk <salahkoyilandy@gmail.com>
2024-08-03 11:10:15 +0530

    delete functionality added for doubly linked list

--- src/dsa_everything/linked_list/doubly_linked_list/DoublyLinkedList.java
+++ src/dsa_everything/linked_list/doubly_linked_list/DoublyLinkedList.java
@@ -4,13 +4,28 @@ public class DoublyLinkedList {
     public static void main(String[] args) {
         DoublyLinkedList list = new DoublyLinkedList();
         list.insertAtEnd(8);
-        list.insertAtEnd(11);
+        list.insertAtEnd(12);
         list.insertAtEnd(19);
         list.insertAtEnd(30);
         list.insertAtEnd(10);
         list.insertAtEnd(11);
 
-        // display nodes
+        // // display nodes from beginning
+        list.traverseFromBeginning();
+
+        // // display nodes from end
+        // list.traverseFromEnd();
+
+        // inserting a value just after 30
+        list.insertAfter(35, 11);
+
+        // display nodes from beginning
+        list.traverseFromBeginning();
+
+        list.insertAtBeginning(1000);
+        list.deleteNode(35);
+
+        // display nodes from beginning
         list.traverseFromBeginning();
     }
 
@@ -39,16 +54,60 @@ public class DoublyLinkedList {
 
     }
 
-    public void insertAfter() {
+    public void insertAfter(int data, int nextTo) {
+        Node newNode = new Node(data);
+        Node tempNode = headNode;
+        while (tempNode != null && tempNode.data != nextTo) {
+            tempNode = tempNode.next;
+        }
 
+        if (tempNode == null) {
+            System.out.println("No next to value found");
+            return;
+        }
+        if (tempNode == tailNode) {
+            newNode.previous = tempNode;
+            tempNode.next = newNode;
+            tailNode = newNode;
+            return;
+        }
+        newNode.next = tempNode.next;
+        tempNode.next = newNode;
+        newNode.previous = tempNode;
+        newNode.next.previous = newNode;
     }
 
-    public void insertAtBeginning() {
-
+    public void insertAtBeginning(int data) {
+        Node newNode = new Node(data);
+        if (headNode == null) {
+            return;
+        }
+        headNode.previous = newNode;
+        newNode.next = headNode;
+        headNode = newNode;
     }
 
-    public void deleteNode() {
-
+    public void deleteNode(int data) {
+        Node currentNode = headNode;
+        while (currentNode != null && currentNode.data != data) {
+            currentNode = currentNode.next;
+        }
+        if (currentNode == null) {
+            System.out.println("Nothing to delete");
+            return;
+        }
+        if (currentNode == headNode) {
+            headNode = headNode.next;
+            headNode.previous = null;
+            return;
+        }
+        if (currentNode == tailNode) {
+            tailNode.previous.next = null;
+            tailNode = tailNode.previous;
+            return;
+        }
+        currentNode.previous.next = currentNode.next;
+        currentNode.next.previous = currentNode.previous;
     }
 
     public void traverseFromBeginning() {
@@ -58,13 +117,23 @@ public class DoublyLinkedList {
         }
         Node currentNode = headNode;
         while (currentNode != null) {
-            System.out.println(currentNode.data);
+            System.out.print(currentNode.data + " ");
             currentNode = currentNode.next;
         }
+        System.out.println();
     }
 
     public void traverseFromEnd() {
-
+        if (tailNode == null) {
+            System.out.println("Linked list is empty");
+            return;
+        }
+        Node currentNode = tailNode;
+        while (currentNode != null) {
+            System.out.print(currentNode.data + " ");
+            currentNode = currentNode.previous;
+        }
+        System.out.println();
     }
 
     public void update() {

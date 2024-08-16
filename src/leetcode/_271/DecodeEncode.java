d30bec6a2bb2b9e73502c7d5a56cab389d4ca344
salahvk <salahkoyilandy@gmail.com>
2024-08-16 08:06:37 +0530

    decode and encode problem solved

--- src/leetcode/_271/DecodeEncode.java
+++ src/leetcode/_271/DecodeEncode.java
@@ -1,25 +1,59 @@
 package leetcode._271;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class DecodeEncode {
     public static void main(String[] args) {
-        List<String> strings = Arrays.asList("neet", "code", "love", "you");
-        // decode(encode(strings));
+        DecodeEncode decodeEncode = new DecodeEncode();
+        List<String> strings = Arrays.asList("we", "say", ":", "yes", "!@#$%^&*()");
+        String encoded = decodeEncode.encode(strings);
+        System.err.println(encoded);
+        List<String> decoded = decodeEncode.decode(encoded);
+        System.err.println(decoded);
     }
 
-    public static String encode(List<String> strs) {
-        StringBuilder stringBuilder = new StringBuilder();
+    public String encode(List<String> strs) {
+        StringBuilder encodedString = new StringBuilder();
 
         for (String string : strs) {
-            stringBuilder.append(string.length() + "#" + string);
+            encodedString.append(string.length() + "#" + string);
         }
-
-        return stringBuilder.toString();
+        return encodedString.toString();
     }
 
-    // public static List<String> decode(String str) {
+    public List<String> decode(String str) {
+        ArrayList<String> decodedStrings = new ArrayList<>();
+        int i = 0;
+        while (i < str.length()) {
+            int j = i;
+            while (str.charAt(j) != '#')
+                j++;
+            int length = Integer.valueOf(str.substring(i, j));
+            i = length + j + 1;
+            decodedStrings.add(str.substring(j + 1, i));
+        }
+        return decodedStrings;
+    }
 
+    // public List<String> decode(String str) {
+    // ArrayList<String> decodedStrings = new ArrayList<>();
+    // int i = 0;
+    // while (i < str.length()) {
+    // if (str.charAt(i) == '#') {
+    // int length = Character.getNumericValue(str.charAt(++i));
+    // decodedStrings.add(str.substring(++i, i += length));
     // }
+    // }
+    // return decodedStrings;
+    // }
+
+    // This is my first solution for decoding string but later it end up in infinity
+    // loop if we check these like array ["we","say",":","yes","!@#$%^&*()"]
+    // also decoding like #5salah instead of 5#salah
+    // i used a function -> int length = Character.getNumericValue(str.charAt(++i));
+    // but the issue is it only take characters and if the character is 10 it take 1
+    // then 0 so that is a problem
+
 }

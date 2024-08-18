c7e6d615e00e92c9f71da170e2ebf2889f346f3c
salahvk <salahkoyilandy@gmail.com>
2024-08-18 10:22:12 +0530

    inorder traversal

---
+++ src/dsa_everything/tree/traversal_of_tree/Inorder.java
@@ -0,0 +1,31 @@
+package dsa_everything.tree.traversal_of_tree;
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class Inorder {
+
+    public static void inorderTraversal(Node node) {
+        if (node == null) {
+            return;
+        }
+        inorderTraversal(node.left);
+        System.out.println(node.data);
+        inorderTraversal(node.right);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        inorderTraversal(root);
+    }
+}

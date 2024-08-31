77fe3e78cedc142836e7a1a6c21a218acec3bd80
salahvk <salahkoyilandy@gmail.com>
2024-08-31 14:11:48 +0530

    max depth leetcode problem solved

---
+++ src/leetcode/_104/Solution.java
@@ -0,0 +1,31 @@
+package leetcode._104;
+
+import dsa_everything.tree.construction.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+
+        if (root == null)
+            return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left, right) + 1;
+
+    }
+}

baafe1b9776502d4bbfd8acfb5b6b8b5d731d429
salahvk <salahkoyilandy@gmail.com>
2024-10-03 22:48:57 +0530

    Buy sell crypto problem solved

--- src/revision/sliding_window/BuySellCrypto.java
+++ src/revision/sliding_window/BuySellCrypto.java
@@ -1,5 +1,18 @@
 package revision.sliding_window;
 
 public class BuySellCrypto {
-    
+    public int maxProfit(int[] prices) {
+        int ans = 0;
+        int l = 0;
+        int r = 1;
+        while (r < prices.length) {
+            if (prices[l] < prices[r]) {
+                ans = Math.max(prices[r] - prices[l], ans);
+            } else {
+                l = r;
+            }
+            r++;
+        }
+        return ans;
+    }
 }
